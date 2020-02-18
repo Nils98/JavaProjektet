15a795a5947f0bdca5bc598d902c8ded6dc98b47
Nils98 <61187621+Nils98@users.noreply.github.com>
2020-02-18 11:49:58 +0100

    Add files via upload

---
+++ Bank/src/BankApplication.java
@@ -0,0 +1,9 @@
+
+public class BankApplication {
+	public static void main(String[] args ) {
+		Customer c1 = new Customer("Nils", 19981012);
+		System.out.println(c1);
+		Customer c2 = new Customer("Tusj", 19981012);
+		System.out.println(c2);
+	}
+}
